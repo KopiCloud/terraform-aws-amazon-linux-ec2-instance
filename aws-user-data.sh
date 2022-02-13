1da45118e2521fc379f085c226aa7855e8897748
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-13 01:01:06 +0100

    Initial commit

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo yum update -y
-sudo yum install -y httpd.x86_64
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 echo "<h1>Test AWS</h1>" | sudo tee /var/www/html/index.html
