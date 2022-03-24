f14c802d77a5b7f1ff16a6b14ab1c1f2567e2179
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-03-24 16:59:16 +0100

    Added support for kernel 5 AMI

--- amazon-linux-versions.tf
+++ amazon-linux-versions.tf
@@ -11,3 +11,13 @@ data "aws_ami" "amazon-linux-2" {
     values = ["amzn2-ami-hvm*"]
   }
 }
+
+data "aws_ami" "amazon-linux-2-kernel-5" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5*"]
+  }
+}
